da913a7f79f1ab54663965e08a066d5b83b09955
Gurpreet Bhoot <gur_boo15@Gurpreets-MacBook-Pro.local>
2018-06-28 10:47:22 -0700

    added iPad constraints using installed button

--- dev-profile/dev-profile/ViewController.swift
+++ dev-profile/dev-profile/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var devLogoImg: UIImageView!
+    @IBOutlet weak var customizeDashboardBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        devLogoImg.layer.cornerRadius = 5.0
+        devLogoImg.layer.masksToBounds = true
+        
+        customizeDashboardBtn.layer.cornerRadius = 5.0
+        customizeDashboardBtn.layer.masksToBounds = true
     }
 
     override func didReceiveMemoryWarning() {
